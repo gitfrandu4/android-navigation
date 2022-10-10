d8d2f3977b5a0c6961048d3edafe0c608d92d6fb
Francisco J <fran.lopez@symbyosys.es>
2022-10-10 08:47:48 +0100

    second commit + update readme

--- app/src/main/java/com/example/navigation/LoginActivity.kt
+++ app/src/main/java/com/example/navigation/LoginActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.navigation.databinding.ActivityLoginBinding
-import com.example.navigation.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
 
